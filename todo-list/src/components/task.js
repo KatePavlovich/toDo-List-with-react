0763219c878af3eb1ed1a3f0b75acb3723672560
KatePavlovich <asylumhere@mail.ru>
2018-10-08 11:52:06 +0300

    fix: checkbox checked

--- todo-list/src/components/task.js
+++ todo-list/src/components/task.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 class Task extends React.Component {
-/*     deleteTask=(e) => {
-        this.props.deleteCallBack(this.)
-    }
- */
+
     render() {
         return (
             <li className="task" id={this.props.id}>
-                <input type="checkbox" onChange={() => this.props.updateCallBack(this.props.task)} />
+                <input type="checkbox" checked={this.props.task.isDone ? 'checked' : ''} onChange={() => this.props.updateCallBack(this.props.task)} />
                 <p className={this.props.task.isDone ? 'done' : ''} >{this.props.task.title}</p>
                 <span className="deleteBtn" onClick={() => this.props.deleteCallBack(this.props.task.id)}>x</span>
             </li>
